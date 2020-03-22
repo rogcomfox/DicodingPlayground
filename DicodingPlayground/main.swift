23df117f29230ec6930e3128b114ddeecd543b71
rogcomfox <joeg.11021996@gmail.com>
2020-03-22 11:04:57 +0700

    intro from me

--- DicodingPlayground/main.swift
+++ DicodingPlayground/main.swift
@@ -1,12 +1,15 @@
-//
-//  main.swift
-//  DicodingPlayground
-//
-//  Created by Joe G on 22/03/20.
 //  Copyright © 2020 Nusantarian. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+print("Selamat Datang, Kawan")
+let firstName = "Budi", lastname = "R"
+let fullName = firstName + " " + lastname
+let adress = "Surabaya"
+let job = "Developer"
+let age = 0b10001
 
+print("Apakah kalian tahu \(fullName)?")
+print("\(firstName) adalah seorang \(job)")
+print("\(firstName) tinggal di \(adress)")
+print("\(firstName) saat ini berumur \(age)")
