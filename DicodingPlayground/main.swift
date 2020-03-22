7fb4373e3ab35bb1591593eddfdfb7a794b7d061
rogcomfox <joeg.11021996@gmail.com>
2020-03-22 10:48:58 +0700

    Initial Commit

---
+++ DicodingPlayground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DicodingPlayground
+//
+//  Created by Joe G on 22/03/20.
+//  Copyright © 2020 Nusantarian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
