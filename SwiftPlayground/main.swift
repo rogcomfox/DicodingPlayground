f3a569c319f827001e8f9fe450e07c1d9472750e
rogcomfox <joeg.11021996@gmail.com>
2020-03-25 10:55:20 +0700

    Add program to calculate Cube

--- SwiftPlayground/main.swift
+++ SwiftPlayground/main.swift
@@ -2,14 +2,5 @@
 
 import Foundation
 
-print("Selamat Datang, Kawan")
-let firstName = "Budi", lastname = "R"
-let fullName = firstName + " " + lastname
-let adress = "Surabaya"
-let job = "Developer"
-let age = 0b10001
-
-print("Apakah kalian tahu \(fullName)?")
-print("\(firstName) adalah seorang \(job)")
-print("\(firstName) tinggal di \(adress)")
-print("\(firstName) saat ini berumur \(age)")
+let cube = Cube()
+cube.run()
